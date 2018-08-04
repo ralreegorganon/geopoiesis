bf1c67b83ed1381eaa303a5a66460ecaaa0a5d96
Jason Jones <jasonedwardjones@gmail.com>
2018-08-03 23:14:36 -0800

    wip

--- doittoit.py
+++ doittoit.py
@@ -2,12 +2,15 @@ import arcpy
 
 arcpy.env.workspace = "H:\cddmap\data\catamasswithstructures.gdb"
 
-omg_fc = "OVERMAP_GRID"
+omg_fc = "in_memory\OVERMAP_GRID"
 subset_grid_fc = "OVERMAP_SUBSET_GRID"
-single_overmap_fc = "SINGLE_OVERMAP"
+single_overmap_fc = "in_memory\SINGLE_OVERMAP"
 
-grid_source_fc = "LANDUSE2005_POLY"
-subset_source_fc = "TOWNSSURVEY_POLYM"
+grid_source_fc = "in_memory\LANDUSE2005_POLY"
+subset_source_fc = "in_memory\TOWNSSURVEY_POLYM"
+
+# arcpy.CopyFeatures_management("LANDUSE2005_POLY", grid_source_fc)
+# arcpy.CopyFeatures_management("TOWNSSURVEY_POLYM", subset_source_fc)
 
 attributes = [
     ("LANDUSE2005_POLY", "LU05_DESC;LUCODE"),
@@ -23,6 +26,9 @@ attributes = [
     ("STRUCTURE_POLY", "STRUCT_ID;AREA_SQ_FT"),
 ]
 
+for source in attributes:
+    arcpy.CopyFeatures_management(source[0], "in_memory\\"+source[0])
+
 recreate_if_exists = True
 
 def nuke(fc):
@@ -30,33 +36,27 @@ def nuke(fc):
         arcpy.Delete_management(fc)
 
 def clip(omid, source_fc):
-    clip_fc = "CLIP_OM%s_%s" % (omid, source_fc)
-    exists = arcpy.Exists(clip_fc)
-
-    if exists and recreate_if_exists:
-        arcpy.Delete_management(clip_fc)
-        arcpy.Clip_analysis(source_fc, single_overmap_fc, clip_fc)
-    elif not exists:
-        arcpy.Clip_analysis(source_fc, single_overmap_fc, clip_fc)
-    
+    clip_fc = "in_memory\CLIP_OM%s" % omid
+    arcpy.Clip_analysis(source_fc, single_overmap_fc, clip_fc)
     return clip_fc
 
 def tabulate(omid, source_fc, clip_fc, fields):
-    tab_fc = "TABULATION_OM%s_%s" % (omid, source_fc)
+    tab_fc = "TABULATION_OM%s_%s" % (omid, source_fc.replace("in_memory\\",""))
     exists = arcpy.Exists(tab_fc)
 
     if exists and recreate_if_exists:
         arcpy.Delete_management(tab_fc)
-        arcpy.TabulateIntersection_analysis(om_fc, "PageNumber", clip_fc, tab_fc, fields)
+        arcpy.TabulateIntersection_analysis(om_fc_m, "PageNumber", clip_fc, tab_fc, fields)
     elif not exists:
-        arcpy.TabulateIntersection_analysis(om_fc, "PageNumber", clip_fc, tab_fc, fields)
+        arcpy.TabulateIntersection_analysis(om_fc_m, "PageNumber", clip_fc, tab_fc, fields)
+
+    arcpy.Delete_management(clip_fc)
 
 def clip_and_tabulate(omid, source_fc, fields):
     clip_fc = clip(omid, source_fc)
     tabulate(omid, source_fc, clip_fc, fields)
 
 print("Creating %s" % omg_fc)
-nuke(omg_fc)
 arcpy.GridIndexFeatures_cartography(omg_fc, grid_source_fc, "NO_INTERSECTFEATURE", "", "", "4320 meters", "4320 meters", "", "", "", "0")
 print("Done creating %s" % omg_fc)
 
@@ -67,7 +67,10 @@ arcpy.SelectLayerByLocation_management("BLARG", "INTERSECT", subset_source_fc)
 arcpy.CopyFeatures_management("BLARG", subset_grid_fc)
 print("Done creating %s" % subset_grid_fc)
 
-with arcpy.da.SearchCursor(subset_grid_fc, "PageNumber", "PageNumber = 1013") as cursor:
+arcpy.Delete_management(omg_fc)
+arcpy.Delete_management("BLARG")
+
+with arcpy.da.SearchCursor(subset_grid_fc, "PageNumber", "PageNumber in (1012, 1013, 1014)") as cursor:
 # with arcpy.da.SearchCursor(subset_grid_fc, "PageNumber") as cursor:
     for row in cursor:
         omid = row[0]
@@ -78,13 +81,18 @@ with arcpy.da.SearchCursor(subset_grid_fc, "PageNumber", "PageNumber = 1013") as
         arcpy.MakeFeatureLayer_management(subset_grid_fc, single_overmap_fc, "PageNumber = %s" % omid)
 
         om_fc = "OVERMAP_TERRAIN_GRID_OM%s" % omid
-        om_fc_exists = arcpy.Exists(om_fc)
+        om_fc_m = "in_memory\\" + om_fc
 
-        if om_fc_exists and recreate_if_exists:
-            arcpy.Delete_management(om_fc)
-            arcpy.GridIndexFeatures_cartography(om_fc, single_overmap_fc, "NO_INTERSECTFEATURE", "", "", "24 meters", "24 meters", "", "180", "180", "0")
-        elif not om_fc_exists:
-            arcpy.GridIndexFeatures_cartography(om_fc, single_overmap_fc, "NO_INTERSECTFEATURE", "", "", "24 meters", "24 meters", "", "180", "180", "0")
+        arcpy.GridIndexFeatures_cartography(om_fc_m, single_overmap_fc, "NO_INTERSECTFEATURE", "", "", "24 meters", "24 meters", "", "180", "180", "0")
 
         for attribute in attributes:
-            clip_and_tabulate(omid, attribute[0], attribute[1])
+            clip_and_tabulate(omid, "in_memory\\"+attribute[0], attribute[1])
+
+        om_fc_exists = arcpy.Exists(om_fc)
+        if om_fc_exists and recreate_if_exists:
+            arcpy.Delete_management(om_fc)
+            arcpy.CopyFeatures_management(om_fc_m, om_fc)
+        elif not om_fc_exists:
+            arcpy.CopyFeatures_management(om_fc_m, om_fc)
+
+        nuke(om_fc_m)
